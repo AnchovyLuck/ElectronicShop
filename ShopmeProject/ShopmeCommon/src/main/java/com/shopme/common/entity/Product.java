fa4fe56cbd550041f9c9e05089fe3c4311faa3a4
Anchovy <anchovyluck20@gmail.com>
2023-09-24 10:31:08 +0700

    Add Currency, Country, State and Customer entity with CRUD test

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -77,6 +77,13 @@ public class Product {
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<ProductDetail> details = new HashSet<>();
 
+	public Product() {
+	}
+
+	public Product(Integer id) {
+		this.id = id;
+	}
+
 	public Integer getId() {
 		return id;
 	}
